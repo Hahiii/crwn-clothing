ead175af6f44f8fd6c5812ec88612444695c2035
Nazim Shala <shalanazim@gmail.com>
2020-02-10 11:33:37 +0100

    updating app with shop page & preview components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
